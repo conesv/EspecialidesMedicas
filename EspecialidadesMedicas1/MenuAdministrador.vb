5588d4f034b29fb9bc4879eb26ed86106b854198
conesv <conesv@gmail.com>
2020-10-30 12:56:08 -0600

    2

--- EspecialidadesMedicas1/MenuAdministrador.vb
+++ EspecialidadesMedicas1/MenuAdministrador.vb
@@ -10,7 +10,7 @@
     Private Sub MenuAdministrador_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
-
+    ''
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         RegistroDeUsuarios.Show()
         Me.Hide()
