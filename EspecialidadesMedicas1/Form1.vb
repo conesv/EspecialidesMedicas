282afd33101d4d59172aca0fc7d6ea22b042ecab
conesv <conesv@gmail.com>
2020-11-19 12:08:59 -0600

    C

--- EspecialidadesMedicas1/Form1.vb
+++ EspecialidadesMedicas1/Form1.vb
@@ -18,4 +18,8 @@
     Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
 
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
