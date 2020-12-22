7ad10fd5ec2768f3ead6cf5f599c01470664c016
conesv <conesv@gmail.com>
2020-12-21 21:48:13 -0600

    Correccion de menus

--- EspecialidadesMedicas1/MenuContador.vb
+++ EspecialidadesMedicas1/MenuContador.vb
@@ -1,7 +1,7 @@
 ﻿Public Class MenuContador
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         ConfirmacionDePagos.Show()
-        Me.Close()
+        Me.Hide()
     End Sub
 
     Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
@@ -10,11 +10,11 @@
 
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
         RegistroDeInformacionDeArrendamiento.Show()
-        Me.Close()
+        Me.Hide()
     End Sub
 
     Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
-        Me.Close()
+        Me.Hide()
         Autenticacion.Show()
     End Sub
 End Class
